1781fff1595705f182620730a2d6d0526a7c66a0
viniciussdahmer <viniciussteindahmer@gmail.com>
2020-05-30 17:36:49 -0300

    feat(level): add more objects and fog to the level

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,11 +15,11 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
